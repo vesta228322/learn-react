eeca51b69b274e330732311d33ceb2e27cd29230
Vesta <hotabych39@gmail.com>
2026-04-04 19:51:24 +0300

    refactoring a component into a class and adding states

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -2,10 +2,30 @@ import {Component} from 'react';
 
 import './employees-list-item.css';
 
-class EmployeesListItem extends Component{
-    
+class EmployeesListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            increase: false,
+            like: false
+        };
+    }
+
+    onIncrease = () => {
+        this.setState(({increase}) => ({
+            increase: !increase
+        }));
+    }
+
+    onLike = () => {
+        this.setState(({like}) => ({
+            like: !like
+        }));
+    }
+
     render() {
-    const {name, salary, increase} = this.props
+    const {name, salary} = this.props;
+    const {increase, like} = this.state;
 
     let classNames = 'list-group-item d-flex justify-content-between';
 
@@ -13,13 +33,18 @@ class EmployeesListItem extends Component{
         classNames += ' increase';
     }
 
+    if (like) {
+        classNames += ' like';
+    }
+
     return (
         <li className={classNames}>
-            <span className="list-group-item-label">{name}</span>
+            <span className="list-group-item-label" onClick={this.onLike}>{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
             <div className="d-flex justify-content-center align-items-center">
                 <button type="button"
-                    className="btn-cookie btn-sm">
+                    className="btn-cookie btn-sm"
+                    onClick={this.onIncrease}>
                         <i className="fas fa-cookie"></i>
                     </button>
 
